ff4834ef5419c7dae5880694c1eaa6454f4710bf
azelio5 <a.alakbarov@gmail.com>
2022-08-17 17:04:49 +0300

    same time

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,22 +7,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
-
-    @Autowired
+    private Animal animal1;
+    private Animal animal2;
     private Timer timer;
+
 
     public Timer getTimer() {
         return timer;
     }
 
+
+
+    @Autowired
+    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer timer) {
+        this.animal1 = animal1;
+        this.animal2 = animal2;
+        this.timer = timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animal1.toString() +", " + animal2.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
